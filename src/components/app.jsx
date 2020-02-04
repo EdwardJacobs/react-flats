87c0f9471aaf0e2b822f263dc979d4c552da75d2
Edward Jacobs <eddielawrence23@gmail.com>
2020-02-04 14:33:38 -0600

    FEAT: static flat component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import Flat from "./flat";
 
+// import flats from "../../data/flats";
+
+
 class App extends Component {
+
   render() {
     return (
       <div>
--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -1,1 +1,13 @@
-flat_list.jsx
+import React, { Component } from 'react';
+import Flat from './flat';
+
+
+class FlatList extends Component {
+  render() {
+    return (
+      <Flat />
+    );
+  }
+}
+
+export default FlatList;
